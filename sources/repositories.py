def244e303766050623b65dfd3787263388205c6
remimd <remi.mandart@gmail.com>
2023-07-07 14:59:27 +0200

    tqt

--- sources/repositories.py
+++ sources/repositories.py
@@ -7,10 +7,10 @@ from sources.query import query
 class ToDoRepository:
     async def get_by_id(self, todo_id: UUID) -> ToDo:
         todo_data = await query.fetch_one(
-            f"SELECT * FROM todo WHERE id = '{todo_id}' LIMIT 1"
+            f"SELECT * FROM `todo` WHERE id = '{todo_id}' LIMIT 1"
         )
         do_list_data = await query.fetch(
-            f"SELECT * FROM do WHERE todo_id = '{todo_id}'"
+            f"SELECT * FROM `do` WHERE todo_id = '{todo_id}'"
         )
         do_list = [Do(id=data["id"], title=data["title"]) for data in do_list_data]
         return ToDo(do_list=do_list, **todo_data)
